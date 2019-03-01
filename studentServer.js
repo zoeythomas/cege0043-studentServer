88db8f55bb997864566efc22a195e13ca8b2c08e
zoeythomas <ucfscde@ucl.ac.uk>
2019-03-01 16:48:21 -0500

    Adding functionality to log the requests

--- studentServer.js
+++ studentServer.js
@@ -1,14 +1,20 @@
-//express is the server that forms part of the nodejs program
+// express is the server that forms part of the nodejs program
 var express = require('express');
 var path = require("path");
-var app = express();
+var app = express()
 // add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files if they are not
 // directly called in a typed URL
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
-
-app.get('/',function(req,res){
-	res.send("Hello world from the HTTP server!");
+app.get('/',function (req,res) {
+res.send("Hello world from the HTTP server!");
+});
+// adding functionality to log the requests
+app.use(function (req, res, next) {
+var filename = path.basename(req.url);
+var extension = path.extname(filename);
+console.log("The file " + filename + " was requested.");
+next();
 });
