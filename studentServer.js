3d791f96daa15512d2a9029e3a804b334dea726a
zoeythomas <ucfscde@ucl.ac.uk>
2019-03-01 16:17:33 -0500

    upload student Server

---
+++ studentServer.js
@@ -0,0 +1,14 @@
+//express is the server that forms part of the nodejs program
+var express = require('express');
+var path = require("path");
+var app = express();
+// add an http server to serve files to the Edge browser
+// due to certificate issues it rejects the https files if they are not
+// directly called in a typed URL
+var http = reuqire('http');
+var httpServer = http.createServer(app);
+httpServer.listen(4480);
+
+app.get('/',function(req,res){
+	res.send("Hello world from the HTTP server!");
+});
