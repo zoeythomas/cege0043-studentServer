18cce767bbfb114dff1a6dfe6a26b070303bd6c3
zoeythomas <ucfscde@ucl.ac.uk>
2019-03-02 19:25:27 -0500

    database connectivity

--- studentServer.js
+++ studentServer.js
@@ -1,3 +1,45 @@
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+app.use(bodyParser.json());
+//Add the body-parser to studentServer.js close to the top so that you will be able to process the uploaded data
+
+
+//Import the required database connectivity code and set up a database connection
+var fs = require('fs');
+var pg = require('pg');
+var configtext =
+""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+// now convert the configruation file into the correct format -i.e. a name/value
+pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+var split = configarray[i].split(':');
+config[split[0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config);
+
+//Add a simple app.get to test out the connection
+app.get('/postgistest', function (req,res) {
+pool.connect(function(err,client,done) {
+if(err){
+console.log("not able to get connection "+ err);2 of 4
+res.status(400).send(err);
+}
+client.query('SELECT name FROM london_poi' ,function(err,result) {
+done();
+if(err){
+console.log(err);
+res.status(400).send(err);
+}
+res.status(200).send(result.rows);
+});
+});
+});
+
+
 // express is the server that forms part of the nodejs program
 var express = require('express');
 var path = require("path");
@@ -10,6 +52,8 @@ app.use(function(req,res,next){
 	res.header("Access-Control-Allow-Headers","X-Requested-With");
 	next();
 });
+//cross origin request
+
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
