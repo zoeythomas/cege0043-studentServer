da50e33fdb0d4d07e9bef36f45dfea3bd0e8ead8
zoeythomas <ucfscde@ucl.ac.uk>
2019-03-06 05:03:54 -0500

    process the uploaded data

--- studentServer.js
+++ studentServer.js
@@ -1,3 +1,18 @@
+
+// express is the server that forms part of the nodejs program
+var express = require('express');
+var path = require("path");
+var app = express()
+// add an http server to serve files to the Edge browser
+// due to certificate issues it rejects the https files if they are not
+// directly called in a typed URL
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "X-Requested-With");
+next();
+});
+//cross origin request
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
 extended: true
@@ -9,8 +24,7 @@ app.use(bodyParser.json());
 //Import the required database connectivity code and set up a database connection
 var fs = require('fs');
 var pg = require('pg');
-var configtext =
-""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
 // now convert the configruation file into the correct format -i.e. a name/value pair array
 var configarray = configtext.split(",");
 var config = {};
@@ -24,7 +38,7 @@ var pool = new pg.Pool(config);
 app.get('/postgistest', function (req,res) {
 pool.connect(function(err,client,done) {
 if(err){
-console.log("not able to get connection "+ err);2 of 4
+console.log("not able to get connection "+ err);
 res.status(400).send(err);
 }
 client.query('SELECT name FROM london_poi' ,function(err,result) {
@@ -38,26 +52,11 @@ res.status(200).send(result.rows);
 });
 });
 
-
-// express is the server that forms part of the nodejs program
-var express = require('express');
-var path = require("path");
-var app = express()
-// add an http server to serve files to the Edge browser
-// due to certificate issues it rejects the https files if they are not
-// directly called in a typed URL
-app.use(function(req,res,next){
-	res.header("Access-Control-Allow-Origin","*");
-	res.header("Access-Control-Allow-Headers","X-Requested-With");
-	next();
-});
-//cross origin request
-
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 app.get('/',function (req,res) {
-res.send("Hello world from the HTTP server!");
+res.send("hello world from the HTTP server");
 });
 // adding functionality to log the requests
 app.use(function (req, res, next) {
