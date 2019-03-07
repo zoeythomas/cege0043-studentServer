f2db8b47bfd89d2c4ebc746454e1237c4c53e8ca
zoeythomas <zoeythomas@163.com>
2019-03-07 10:48:15 -0500

    make the GeoJSON serer more flexiable

--- studentServer.js
+++ studentServer.js
@@ -126,6 +126,65 @@ res.status(200).send(result.rows);
 });
 })
 
+//multiple end points server can deal with different situations(e.g. in case change div you enter nothing)
+app.get('/',function(req,res){
+	res.send('hello world!');
+});
+
+app.get('/getGeoJSON/:tablename/:geomcolumn/:portNumber?', function (req,res) {
+pool.connect(function(err,client,done) {
+if(err){
+console.log("not able to get connection "+ err);
+res.status(400).send(err);
+}
+var colnames = "";
+// first get a list of the columns that are in the table
+// use string_agg to generate a comma separated list that can then be pasted into the next query
+var tablename = req.params.tablename;
+var geomcolumn = req.params.geomcolumn;
+var querystring = "select string_agg(colname,',') from ( select column_name as colname ";
+querystring = querystring + " FROM information_schema.columns as colname ";
+querystring = querystring + " where table_name =$1";
+querystring = querystring + " and column_name <> $2 and data_type <> 'USER-DEFINED') as cols ";
+console.log(querystring);
+// now run the query
+client.query(querystring,[tablename,geomcolumn], function(err,result){
+//call `done()` to release the client back to the pool
+done();
+if(err){
+console.log(err);
+res.status(400).send(err);
+}
+thecolnames = result.rows[0].string_agg;
+colnames = thecolnames;
+console.log("the colnames "+thecolnames);
+// now use the inbuilt geoJSON functionality
+// and create the required geoJSON format using a query adapted from here:
+// http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+// note that query needs to be a single string with no line breaks so built it up bit by bit
+var querystring = " SELECT 'FeatureCollection' As type,array_to_json(array_agg(f)) As features FROM ";
+querystring = querystring + "(SELECT 'Feature' As type ,ST_AsGeoJSON(lg." + req.params.geomcolumn+")::json As geometry, ";
+querystring = querystring + "row_to_json((SELECT l FROM (SELECT"+colnames + ") As l )) As properties";
+// depending on whether we have a port number, do differen things
+if (req.params.portNumber) {
+querystring = querystring + " FROM "+req.params.tablename+"As lg where lg.port_id = '"+req.params.portNumber + "' limit 100 ) As f ";
+}
+else {
+querystring = querystring + " FROM "+req.params.tablename+"As lg limit 100 ) As f ";
+}
+console.log(querystring);
+// run the second query
+client.query(querystring,function(err,result){
+//call `done()` to release the client back to the pool
+done();
+if(err){
+console.log(err);
+res.status(400).send(err);
+}
+res.status(200).send(result.rows);
+});
+});
+
 // serve static files - e.g. html, css
 // this should always be the last line in the server file
 app.use(express.static(__dirname));
