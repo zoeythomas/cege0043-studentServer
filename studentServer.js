29d8a1a2efdba596fe3cbf877a84778ff0dca316
zoeythomas <zoeythomas@163.com>
2019-03-07 11:06:11 -0500

    make the GeoJSON serer more flexiable

--- studentServer.js
+++ studentServer.js
@@ -131,35 +131,6 @@ app.get('/',function(req,res){
 	res.send('hello world!');
 });
 
-//get data from database
-app.get('/getFormData/:port_id', function (req,res) {
-pool.connect(function(err,client,done) {
-if(err){
-console.log("not able to get connection "+ err);
-res.status(400).send(err);
-}
-// use the inbuilt geoJSON functionality
-// and create the required geoJSON format using a query adapted from here: http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-FeatureCollections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
-// note that query needs to be a single string with no line breaks so built it up bit by bit
-
-var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ";
-querystring = querystring + "(SELECT 'Feature' As type , ST_AsGeoJSON(lg.geom)::json As geometry, ";
-querystring = querystring + "row_to_json((SELECT l FROM (SELECT name, surname, port_id) As l ";
-querystring = querystring + " )) As properties";
-querystring = querystring + " FROM formdata As lg where lg.port_id = '"+req.params.port_id + "' limit 100 ) As f ";
-console.log(querystring);
-client.query(querystring,function(err,result){
-//call `done()` to release the client back to the pool
-done();
-if(err){
-console.log(err);
-res.status(400).send(err);
-}
-res.status(200).send(result.rows);
-});
-});
-});
-
 //get a certain column from a certain table
 app.get('/getGeoJSON/:tablename/:geomcolumn/:portNumber?', function (req,res) {
 pool.connect(function(err,client,done) {
@@ -179,7 +150,7 @@ querystring = querystring + " and column_name <> $2 and data_type <> 'USER-DEFIN
 console.log(querystring);
 // now run the query
 client.query(querystring,[tablename,geomcolumn], function(err,result){
-//call 'done()'' to release the client back to the pool
+//call `done()` to release the client back to the pool
 done();
 if(err){
 console.log(err);
@@ -190,17 +161,17 @@ colnames = thecolnames;
 console.log("the colnames "+thecolnames);
 // now use the inbuilt geoJSON functionality
 // and create the required geoJSON format using a query adapted from here:
-// http://www.postgresonline.com/journal/archives/267-CreatingGeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+// http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
 // note that query needs to be a single string with no line breaks so built it up bit by bit
-var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ";
-querystring = querystring + "(SELECT 'Feature' As type , ST_AsGeoJSON(lg." + req.params.geomcolumn+")::json As geometry, ";
-querystring = querystring + "row_to_json((SELECT l FROM (SELECT "+colnames + ") As l )) As properties";
+var querystring = " SELECT 'FeatureCollection' As type,array_to_json(array_agg(f)) As features FROM ";
+querystring = querystring + "(SELECT 'Feature' As type ,ST_AsGeoJSON(lg." + req.params.geomcolumn+")::json As geometry, ";
+querystring = querystring + "row_to_json((SELECT l FROM (SELECT"+colnames + ") As l )) As properties";
 // depending on whether we have a port number, do differen things
 if (req.params.portNumber) {
-querystring = querystring + " FROM " + req.params.tablename + " As lg where lg.port_id = '" + req.params.portNumber + "' limit 100 ) As f ";
+querystring = querystring + " FROM "+req.params.tablename+"As lg where lg.port_id = '"+req.params.portNumber + "' limit 100 ) As f ";
 }
 else {
-querystring = querystring + " FROM " + req.params.tablename + " As lg limit 100 ) As f ";
+querystring = querystring + " FROM "+req.params.tablename+"As lg limit 100 ) As f ";
 }
 console.log(querystring);
 // run the second query
